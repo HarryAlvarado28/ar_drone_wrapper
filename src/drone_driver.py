c5f36625ad701cbc6d952ba619b25daca41cc0d8
Ariel Vernaza <ariel.darkserver@gmail.com>
2018-11-25 20:42:12 -0500

    drone driver

---
+++ src/drone_driver.py
@@ -0,0 +1,14 @@
+#This module export all the topics needed for the drone work
+# /ardrone/cmd_vel
+# /ardrone/odom
+# /ardrone/front_camera/raw_image
+# /ardrone/front_camera/raw_image_compressed
+# /ardrone/altitude
+# /ardrone/battery
+# /ardrone/tf
+# /ardrone/nav_data
+# /ardrone/takeoff
+# /ardrone/land
+# /ardrone/reset
+# /ardrone/mag
+# Tag detection, I will work on it in the next version
