cc5cd0d5531fca72352bdff9c62d26b57d757bd3
Ariel Vernaza <ariel.darkserver@gmail.com>
2018-11-25 20:57:37 -0500

    src basic structure

--- src/drone_driver.py
+++ src/drone_driver.py
@@ -1,4 +1,8 @@
-#This module export all the topics needed for the drone work
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyleft (c) 2018 CITIC.
+## This module export all the topics needed for the drone work
 # /ardrone/cmd_vel
 # /ardrone/odom
 # /ardrone/front_camera/raw_image
@@ -12,3 +16,52 @@
 # /ardrone/reset
 # /ardrone/mag
 # Tag detection, I will work on it in the next version
+
+# Authors:
+#   * Ariel Vernaza (DSAPANDORA)
+#     avernaza@citicup.org
+import sys
+from std_msgs.msg import String
+from std_msgs.msg import Empty
+from sensor_msgs.msg import CompressedImage
+from geometry_msgs.msg import Twist
+# import the necessary packages
+from imutils import paths
+import numpy as np
+import imutils
+import cv2
+from libardrone import ardrone
+
+
+def cmd_vel(move_data):
+    # TODO
+
+def front_camera_image():
+    # TODO
+
+def odom():
+    # TODO
+
+def altitude():
+    # TODO
+
+def battery():
+    # TODO
+
+def tf():
+    # TODO
+
+def nav_data():
+    # TODO
+
+def takeoff(data):
+    # TODO
+
+def land(data):
+    # TODO
+
+def reset(data):
+    # TODO
+
+def mag():
+    # TODO
